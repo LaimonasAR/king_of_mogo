0b2aeecc3abb006c4d127e75d6f39a032862272c
Arturas Ardzijauskas <arturas.ardzijauskas@gmail.com>
2023-05-15 20:57:20 +0300

    edit main.py

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ print("*************")
 print("Welcome")
 print("*************")
 
+
+
 while True:
 
     choice = input("1 - enter task\n2 - find task\n3 - update task\n4 - delete task\n0 - exit\n")
 
     match choice:
         case '1':
-            task_from_user = input("Plese enter task name: ")
-            app.create_task(task_from_user, query = {"name": {task_from_user}})
+            user_name = input("Plese enter user name: ")
+            task_title = input("Plese enter task title: ")
+            task_description = input("Plese enter task desription: ")
+            task_status = input("Plese enter task status: ")
+            app.create_task({"name": user_name, "task_title": task_title, "task_desription": task_description, "task_status": task_status})
             
         case '2': 
             find_task = input("Enter task to find: ")
-            app.update_task(find_task, query = {"name": {find_task}})
+            app.update_task(query = {"name": {find_task}})
         
         case '3':
             update_task = input("Enter task to update: ") 
-            app.delete_task(update_task, query = {"name": {update_task}})
+            app.delete_task(query = {"task_title": {update_task}})
 
         case '4':
             del_task = input("Enter task to delete: ") 
-            app.delete_task(del_task, query = {"name": {del_task}})
+            app.delete_task(query = {"name": {del_task}})
 
         case '0':
             print("Goodbye")
