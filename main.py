3c0ded2171fd434b92fe9077de9303bac2efb59e
Arturas Ardzijauskas <arturas.ardzijauskas@gmail.com>
2023-05-16 19:19:36 +0300

    Edit function names and choice input meniu

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print("*************")
 
 while True:
 
-    choice = input("1 - enter task\n2 - find task\n3 - update task\n4 - delete task\n0 - exit\n")
+    choice = input("1 - enter task\n2 - find task\n3 - update by task title\n4 - delete task by name\n0 - exit\n")
 
     match choice:
         case '1':
@@ -19,11 +19,11 @@ while True:
             
         case '2': 
             find_task = input("Enter task to find: ")
-            app.update_task(query = {"name": user_name, "task_title": find_task})
+            app.find_task(query = {"name": user_name, "task_title": find_task})
         
         case '3':
             update_task = input("Enter task to update: ") 
-            app.delete_task(query = {"task_title": update_task})
+            app.update_task(query = {"task_title": update_task})
 
         case '4':
             del_task = input("Enter task to delete: ") 
